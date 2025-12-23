b20b6d5dd5ce5853b767860f4e6bf3c0155a38f2
Mahfuj-HSTU <rmahfujur16@gmail.com>
2025-12-23 21:08:47 +0600

    add queries

---
+++ queries.sql
@@ -0,0 +1,36 @@
+-- query 1
+SELECT
+  b.booking_id,
+  u.name AS customer_name,
+  v.name AS vehicle_name,
+  b.start_date,
+  b.end_date,
+  b.status
+FROM "Bookings" b
+JOIN "Users" u ON b.user_id = u.user_id
+JOIN "Vehicles" v ON b.vehicle_id = v.vehicle_id
+WHERE u.role = 'Customer';
+
+-- query 2
+SELECT *
+FROM "Vehicles"
+WHERE status NOT IN ('rented');
+
+-- query 3
+SELECT *
+FROM "Vehicles"
+WHERE status = 'available'
+  AND LOWER(type) = 'car';
+
+-- query 4
+SELECT
+  v.vehicle_id,
+  v.name AS vehicle_name,
+  COUNT(b.booking_id) AS total_bookings
+FROM "Vehicles" v
+JOIN "Bookings" b
+  ON v.vehicle_id = b.vehicle_id
+GROUP BY
+  v.vehicle_id,
+  v.name
+HAVING COUNT(b.booking_id) > 2;
